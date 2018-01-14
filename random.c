9711b857a1762f6febc1afb72f299f925a2b0e48
j000 <jarymut+github@gmail.com>
2018-01-14 12:15:40 +0100

    Fallback to /dev/urandom for random seed

--- random.c
+++ random.c
@@ -1,6 +1,7 @@
 #include "random.h"
 
 #include <stdlib.h> /* rand, srand, RAND_MAX */
+#include <stdio.h>
 #if defined (__has_include) && __has_include("sys/random.h")
 #   include <sys/random.h>
 #else /* if defined (__has_include) && __has_include("sys/random.h") */
@@ -18,7 +19,8 @@ static double generate_random(void) {
 /**
  * fallback:
  * 1) getrandom (jeśli dostępny podczas kompilacji)
- * 2) domyślna wartość
+ * 2) odczyt z /dev/urandom
+ * 3) domyślna wartość
  */
 static double init_random(void) {
 	unsigned int seed = 0;
@@ -27,8 +29,16 @@ static double init_random(void) {
 	if (getrandom(&seed, sizeof(seed), 0) == -1)
 #endif /* ifndef NO_GETRANDOM */
 	{
-		/* domyślna wartość */
-		seed = 0xFEEDu;
+		/* otwórz /dev/urandom */
+		FILE *urandom = fopen("/dev/urandom", "r");
+
+		/* i przeczytaj kilka bajtów */
+		if (urandom != NULL) {
+			if (fread(&seed, sizeof(seed), 1, urandom) != 1)
+				/* domyślna wartość */
+				seed = 0xFEEDu;
+			fclose(urandom);
+		}
 	}
 
 	/* zainicjuj generator */
