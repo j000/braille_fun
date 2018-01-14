c4e540ee53eb205567a752db1ef949cae64ba44e
j000 <jarymut+github@gmail.com>
2018-01-14 11:41:26 +0100

    Seed random number generator with our value

--- random.c
+++ random.c
@@ -6,7 +6,22 @@
  * Zwróć wartość [0,1]
  * TODO: może [0,1)?
  */
-double get_random(void) {
+static double generate_random(void) {
 	/* TODO: znajdź lepsze rozwiązanie */
 	return ((double)rand()) / RAND_MAX;
 }
+
+static double init_random(void) {
+	/* domyślna wartość */
+	unsigned int seed = 0xFEEDu;
+
+	/* zainicjuj generator */
+	srand(seed);
+
+	/* zmień wskaźnik */
+	get_random = generate_random;
+	/* wygeneruj liczbę */
+	return generate_random();
+}
+
+double (*get_random)(void) = init_random;
--- random.h
+++ random.h
@@ -1,5 +1,5 @@
 #ifndef RANDOM_H
 #define RANDOM_H
 
-double get_random(void);
+double (*get_random)(void);
 #endif /* RANDOM_H */
