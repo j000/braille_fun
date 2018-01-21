ea4538b66cdbbf91ae36ad14c03bad4fadc9b2d2
j000 <jarymut+github@gmail.com>
2018-01-21 23:31:50 +0100

    Add proper flocking behaviour

--- boids.c
+++ boids.c
@@ -7,6 +7,23 @@
 
 /* **** */
 
+/* defaults */
+static const double range = 5.;
+static const double max_speed = 1.5;
+static const double max_acceleration = 0.2;
+static const double separation = 3.;
+static const double edge_distance = 2.;
+
+static const double wander_angle_change = M_PI / 180. * 30;
+static const double wander_distance = 0.3;
+
+static const double wander_weight = 0.1;
+static const double alignment_weight = 0.4;
+static const double cohesion_weight = 0.3;
+static const double separation_weight = 2.0;
+
+/* **** */
+
 typedef struct {
 	double x;
 	double y;
@@ -15,6 +32,8 @@ typedef struct {
 typedef struct {
 	vector pos;
 	vector vel;
+	vector acc;
+	double wander_angle;
 } boid;
 
 struct boids_model_s {
@@ -139,6 +158,7 @@ static void initialise_boids(const size_t n, boid a[n], int x, int y) {
 			.x = -1. + get_random() * 2,
 			.y = -1. + get_random() * 2
 		};
+		a[i].acc = (vector){0 };
 	}
 }
 
@@ -165,26 +185,145 @@ void boids_destroy(boids_model *boids) {
 void boids_update(screen_t screen, boids_model boids) {
 	sort(boids->array, boids->n, compare_x);
 
+	/* sprawdzamy tylko przedział (x - range; x + range)
+	 * i przesuwamy go dla każdego boida */
+	size_t lo = 0, hi = 0;
+
 	for (size_t i = 0; i < boids->n; ++i) {
 		boid *b = &boids->array[i];
+
+		/* popraw przedział */
+		while (lo < boids->n - 1 &&
+			boids->array[lo].pos.x + range < b->pos.x)
+			++lo;
+		while (hi < boids->n &&
+			boids->array[hi].pos.x - range <= b->pos.x)
+			++hi;
+
+		/* Wander */
+		vector wander_v = { 0 };
+		{
+			wander_v.x += wander_distance * cos(b->wander_angle);
+			wander_v.y += wander_distance * sin(b->wander_angle);
+			b->wander_angle += (get_random() - 0.5) * wander_angle_change;
+		}
+
+		b->acc.x += wander_v.x * wander_weight;
+		b->acc.y += wander_v.y * wander_weight;
+
+		/* Alignment */
+		vector alignment_v = { 0 };
+		unsigned int alignment_c = 0;
+
+		/* Cohesion */
+		vector cohesion_v = { 0 };
+		unsigned int cohesion_c = 0;
+
+		/* Separation */
+		vector separation_v = { 0 };
+
+		for (size_t j = lo; j < hi; ++j) {
+			if (i == j)
+				continue;
+
+			vector out = boids->array[j].pos;
+
+			out.x -= b->pos.x;
+			out.y -= b->pos.y;
+
+			double dist_sqared = out.x * out.x + out.y * out.y;
+
+			if (dist_sqared >= (range * range))
+				continue;
+
+			if (dist_sqared <= separation) {
+				/* double tmp = (sqrt(dist_sqared) / boids->separation) - 1.; */
+				double tmp = -1. / dist_sqared;
+
+				separation_v.x += out.x * tmp;
+				separation_v.y += out.y * tmp;
+			} else {
+				alignment_v.x += boids->array[j].vel.x;
+				alignment_v.y += boids->array[j].vel.y;
+				++alignment_c;
+
+				cohesion_v.x += boids->array[j].pos.x;
+				cohesion_v.y += boids->array[j].pos.y;
+				++cohesion_c;
+			}
+		}
+
+		if (alignment_c > 0) {
+			alignment_v.x *= 1. / alignment_c;
+			alignment_v.y *= 1. / alignment_c;
+			alignment_v.x -= b->vel.x;
+			alignment_v.y -= b->vel.y;
+
+			b->acc.x += alignment_v.x * alignment_weight;
+			b->acc.y += alignment_v.y * alignment_weight;
+		}
+
+		if (cohesion_c > 0) {
+			cohesion_v.x *= 1. / cohesion_c;
+			cohesion_v.y *= 1. / cohesion_c;
+			cohesion_v.x -= b->pos.x;
+			cohesion_v.y -= b->pos.y;
+
+			b->acc.x += cohesion_v.x * cohesion_weight;
+			b->acc.y += cohesion_v.y * cohesion_weight;
+		}
+
+		b->acc.x += separation_v.x * separation_weight;
+		b->acc.y += separation_v.y * separation_weight;
+
+		/* borders */
+		if (b->pos.x + edge_distance >= screen_get_x(screen))
+			b->acc.x -= (max_speed - b->vel.x);
+		else if (b->pos.x - edge_distance <= 0)
+			b->acc.x += max_speed - b->vel.x;
+		if (b->pos.y + edge_distance >= screen_get_y(screen))
+			b->acc.y -= max_speed - b->vel.y;
+		else if (b->pos.y - edge_distance <= 0)
+			b->acc.y += max_speed - b->vel.y;
+
+		/* acceleration limit */
+		{
+			double l = b->acc.x * b->acc.x + b->acc.y * b->acc.y;
+
+			if (l > 0 && l > max_acceleration * max_acceleration) {
+				b->acc.x *= max_acceleration / sqrt(l);
+				b->acc.y *= max_acceleration / sqrt(l);
+			}
+		}
+	}
+
+	for (size_t i = 0; i < boids->n; ++i) {
+		boid *b = &boids->array[i];
+
+		/* update */
+		b->vel.x += b->acc.x;
+		b->vel.y += b->acc.y;
+
+		b->pos.x += b->vel.x;
+		b->pos.y += b->vel.y;
+
+		b->acc.x = 0;
+		b->acc.y = 0;
+
+		/* speed limit */
+		{
+			double l = b->vel.x * b->vel.x + b->vel.y * b->vel.y;
+
+			if (l > 0 && l > max_speed * max_speed) {
+				b->vel.x *= max_speed / sqrt(l);
+				b->vel.y *= max_speed / sqrt(l);
+			}
+		}
+
+		/* draw */
 		int tmp_x = floor(b->pos.x);
 		int tmp_y = floor(b->pos.y);
 
 		screen_add_dot(screen, tmp_x, tmp_y);
-
-		/* ruch */
-		b->pos.x += b->vel.x;
-		b->pos.y += b->vel.y;
-		/* odbicia */
-		if ((b->pos.x >= screen_get_x(screen) && b->vel.x > 0) ||
-			(b->pos.x < 0 && b->vel.x < 0)) {
-			b->vel.x *= -1.;
-			b->pos.x += 2 * b->vel.x;
-		}
-		if ((b->pos.y >= screen_get_y(screen) && b->vel.y > 0) ||
-			(b->pos.y < 0 && b->vel.y < 0)) {
-			b->vel.y *= -1.;
-			b->pos.y += 2 * b->vel.y;
-		}
 	}
 }
